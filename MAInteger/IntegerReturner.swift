aab96fb3c43f73e069b6b5b36313ff24e981e3cb
Erik Brandsma <erik@mac2.local>
2016-04-01 12:44:23 +0200

    Initial commit

---
+++ MAInteger/IntegerReturner.swift
@@ -0,0 +1,17 @@
+//
+//  IntegerReturner.swift
+//  MAInteger
+//
+//  Created by Erik Brandsma on 01/04/16.
+//  Copyright © 2016 Jip. All rights reserved.
+//
+
+import Foundation
+
+public class IntegerReturner {
+    
+    public static func getInt(a:Int, b:Int) -> Int {
+        return a+b;
+    }
+    
+}
---
+++ MAInteger/MAInteger.h
@@ -0,0 +1,19 @@
+//
+//  MAInteger.h
+//  MAInteger
+//
+//  Created by Erik Brandsma on 01/04/16.
+//  Copyright © 2016 Jip. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for MAInteger.
+FOUNDATION_EXPORT double MAIntegerVersionNumber;
+
+//! Project version string for MAInteger.
+FOUNDATION_EXPORT const unsigned char MAIntegerVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <MAInteger/PublicHeader.h>
+
+
